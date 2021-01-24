97bdb7bdc948bf10800e0363fd94ce9fe0bc7ebf
Nat Karmios <nat@karmios.com>
2021-01-24 01:54:48 +0000

    Add CREATED property alongside new headers in org-mode

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -119,3 +119,31 @@
 
 (add-to-list 'company-backends 'company-keywords)
 (add-to-list 'company-backends 'company-capf)
+
+;; https://stackoverflow.com/questions/12262220/add-created-date-property-to-todos-in-org-mode
+;; Allow automatically handing of created/expired meta data.
+(require 'org-expiry)
+;; Configure it a bit to my liking
+(setq
+  org-expiry-created-property-name "CREATED" ; Name of property when an item is created
+  org-expiry-inactive-timestamps   t         ; Don't have everything in the agenda view
+)
+
+(defun custom/insert-created-timestamp()
+  "Insert a CREATED property using org-expiry.el for TODO entries"
+  (org-expiry-insert-created)
+  (org-back-to-heading)
+  (org-end-of-line)
+  (insert " ")
+  (evil-insert 0)
+  (evil-delete-backward-char-and-join 1)
+)
+
+;; Whenever a TODO entry is created, I want a timestamp
+;; Advice org-insert-todo-heading to insert a created timestamp using org-expiry
+(defadvice org-insert-heading (after custom/created-timestamp-advice activate)
+  "Insert a CREATED property using org-expiry.el for TODO entries"
+  (custom/insert-created-timestamp)
+)
+;; Make it active
+(ad-activate 'org-insert-heading)
